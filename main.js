e066088a0bc740044ac06e35da1c52adb7846115
Will Gallia <wgallia@gmail.com>
2015-08-24 18:39:53 +0100

    tabs -> spaces in main.js

--- main.js
+++ main.js
@@ -4,50 +4,50 @@ $(document).ready(function(){
     (function drawmap() {
 
         $.getJSON("points.php", function(data) { 
-      	    
-      	    var coordinates = data.map(function(e) {
+
+            var coordinates = data.map(function(e) {
                 return new google.maps.LatLng(e.lat, e.lon);
             });
-            
-      	    var path = new google.maps.Polyline({
-			    path: coordinates,
-			    strokeColor: "#ff3195", //hot pink!!!
-			    strokeOpacity: 1.0,
-			    strokeWeight: 2,
-		    });
+
+            var path = new google.maps.Polyline({
+                path: coordinates,
+                strokeColor: "#ff3195", //hot pink!!!
+                strokeOpacity: 1.0,
+                strokeWeight: 2,
+            });
 
             var mapOptions = {
                 center: coordinates[coordinates.length - 1],
-	            zoom: 10,
-	            mapTypeId: google.maps.MapTypeId.ROADMAP
-	        };
-            
-	        map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-		    
-		    path.setMap(map);
-		    
-		    marker = new google.maps.Marker({
-			    position: coordinates[coordinates.length - 1],
-			    map: map,
-			    animation: google.maps.Animation.DROP,
+                zoom: 10,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+
+            map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+
+            path.setMap(map);
+
+             marker = new google.maps.Marker({
+                position: coordinates[coordinates.length - 1],
+                map: map,
+                animation: google.maps.Animation.DROP,
                 title: "this is a ttle",
                 url: "http://fezz.in",
-		    });
+            });
 
             google.maps.event.addListener(marker, 'click', function() {
                 window.location.href = this.url;
             });
-		    
-		    // var emarker = new google.maps.Marker({
-		    //     position: new google.maps.LatLng(48.135281,11.56758),
-		    //     map: map,
-		    //     animation: google.maps.Animation.DROP,
-		    //     title: "The final destination"
+
+            // var emarker = new google.maps.Marker({
+            //     position: new google.maps.LatLng(48.135281,11.56758),
+            //     map: map,
+            //     animation: google.maps.Animation.DROP,
+            //     title: "The final destination"
             // });
-		    var date = new Date(data[data.length-1].time * 1000.0);
-		    var lastupdate = document.getElementById("lastupdate");
-		    lastupdate.innerHTML = "Last update: " + date;
-      	    
+            var date = new Date(data[data.length-1].time * 1000.0);
+            var lastupdate = document.getElementById("lastupdate");
+            lastupdate.innerHTML = "Last update: " + date;
+
         });
 
     })();
